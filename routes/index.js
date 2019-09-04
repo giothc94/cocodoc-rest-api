67e996c88569430a60991123b697c7821e634cdd
Gabriel Arguello Costta <gio666nb@gmail.com>
2019-09-04 13:34:01 -0500

    removiendo google_cloud test

--- routes/index.js
+++ routes/index.js
@@ -4,18 +4,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    const Firestore = require('@google-cloud/firestore');
-    const db = new Firestore({
-        projectId: 'cocodoc-1563257331101',
-        keyFilename: `/home/giothcode/Escritorio/cocodocREST/Cocodoc-1738ee1630b1.json`,
-    })
-    let docRef = db.collection('users').doc('alovelace');
-
-    let setAda = docRef.set({
-        first: 'Ada',
-        last: 'Lovelace',
-        born: 1815
-    });
     res.render('index', { title: 'Express' });
 });
 
